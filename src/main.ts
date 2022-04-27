c17a912954ea80437059c8201b386a800c216d25
patrickmwangi017 <mwangipatrick017@gmail.com>
2022-04-27 11:34:55 +0300

    day one and day two progress

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import AppSecond from './components/Portfolio.vue';
 
 const app = createApp(App);
-
+const appSecond = createApp(AppSecond);
 app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+appSecond.mount("#appSecond");
